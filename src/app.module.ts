c7b4a4617c39074ec7a0114184fe54bdd80e99d8
Raphael Vignau <gondawraf@hotmail.fr>
2021-12-06 16:30:51 +0100

    Mise a jour v8

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { HelmetMiddleware } from '@nest-middlewares/helmet';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
-// import { MailerModule } from '@nestjs-modules/mailer';
-// import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 import { BottlesModule } from './bottles/bottles.module';
 import { MaterialModule } from './materials/material.module';
 import { PassageModule } from './passages/passage.module';
@@ -28,34 +26,33 @@ import { OrdersModule } from './orders/orders.module';
             // TODO ATTENTION passer à "false" en prod
             synchronize: true
         }),
-        // MailerModule.forRoot({
-        //     transport: {
-        //         host: process.env.MAILER_HOST,
-        //         secure: false,
-        //         auth: {
-        //             user: process.env.MAILER_USER,
-        //             pass: process.env.MAILER_PASSWORD
-        //         },
-        //     },
-        //     defaults: {
-        //         from: process.env.MAILER_FROM
-        //     },
-        //     template: {
-        //         dir: __dirname + '/templates',
-        //         adapter: new PugAdapter(),
-        //         options: {
-        //             strict: true
-        //         }
-        //     }
-        // }),
+        MailerModule.forRoot({
+            transport: {
+                host: process.env.MAILER_HOST,
+                secure: false,
+                auth: {
+                    user: process.env.MAILER_USER,
+                    pass: process.env.MAILER_PASSWORD
+                },
+            },
+            defaults: {
+                from: process.env.MAILER_FROM
+            },
+            template: {
+                dir: __dirname + '/templates',
+                adapter: new PugAdapter(),
+                options: {
+                    strict: true
+                }
+            }
+        }),
         AuthModule,
         BottlesModule,
         MaterialModule,
         PassageModule,
         OrdersModule
     ],
-    controllers: [AppController],
-    providers: [AppService]
+    controllers: [AppController]
 })
 export class AppModule implements NestModule {
     configure(consumer: MiddlewareConsumer): any {
